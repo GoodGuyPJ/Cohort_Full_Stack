3ea5fe2f781bdae016fed260749f825abfc42374
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-03-06 22:13:21 +0530

    express learn

--- JSPractice/express/app.js
+++ JSPractice/express/app.js
@@ -9,7 +9,29 @@
 
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.post("/api/v1/register", (req, res) => {
+  const userName = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  res.json({
+    success: true,
+    message: "User registered successfully",
+  });
+});
+
+// REST API is a way for applications to communicate with each other over the internet using HTTP methods like GET, POST, PUT, DELETE, etc. It stands for Representational State Transfer and is commonly used to build web services.
+
+// RESTful API is an architectural style for an application program interface (API) that uses HTTP requests to access and use data. That data can be used to GET, PUT, POST, and DELETE data types, which refers to the reading, updating, creating, and deleting of operations concerning resources.
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
