9374059c04083e8c953e5545cbbca3724371dbbb
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-03-10 22:57:50 +0530

    LEARNING

--- JSPractice/basic/index.js
+++ JSPractice/basic/index.js
@@ -1,1 +1,5 @@
 // new learning step by launching a new project
+// const, let, var
+// var is function scoped and let is block scoped and const is block scoped
+// var can be redeclared and redefined but let and const can't be redeclared but can be redefined
+console.log("Hello World");
