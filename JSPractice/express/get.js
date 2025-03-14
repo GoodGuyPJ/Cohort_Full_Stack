29d6ae2c959bed836a23591badf458b093b7381e
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-03-14 16:56:47 +0530

    get express

---
+++ JSPractice/express/get.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Define a simple GET route
+app.get("/", (req, res) => {
+  res.send("Hello, this is a GET request!");
+});
+
+// Define a route with a parameter
+app.get("/greet/:name", (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}!`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
