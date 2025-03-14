5180e2933d80760a5d3e12d8f651f82cb490013a
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-03-14 16:55:01 +0530

    express

--- JSPractice/express/app.js
+++ JSPractice/express/app.js
@@ -1,26 +1,26 @@
-// import a from "./index";
-// const a = require("./index.js");
+// // import a from "./index";
+// // const a = require("./index.js");
 
-// a.avrage(2, 3);
-// a.sum(2, 3);
+// // a.avrage(2, 3);
+// // a.sum(2, 3);
 
-// console.log(a.avrage(2, 3));
-// console.log(a.sum(2, 3));
+// // console.log(a.avrage(2, 3));
+// // console.log(a.sum(2, 3));
 
-const express = require("express");
-const app = express();
-const path = require("path");
-const port = 3000;
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
-const router = require("./userRoutes");
-app.use("/api/v1", router);
-// app.use();
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const port = 3000;
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.json());
+// const router = require("./userRoutes");
+// app.use("/api/v1", router);
+// // app.use();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/index.html"));
+// });
 
 // app.post("/api/v1/register", (req, res) => {
 //   const userName = req.body.name;
@@ -40,6 +40,36 @@ app.get("/", (req, res) => {
 
 // RESTful API is an architectural style for an application program interface (API) that uses HTTP requests to access and use data. That data can be used to GET, PUT, POST, and DELETE data types, which refers to the reading, updating, creating, and deleting of operations concerning resources.
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+//server.js
+// Import the express module
+
+const express = require("express");
+
+// Create an instance of the express application
+const app = express();
+
+// Define a route for the root URL ("/")
+app.get("/", (req, res) => {
+  res.send("Hello, this is the root/main route!");
+});
+
+// Define another route for "/api" with JSON response
+app.get("/api", (req, res) => {
+  res.json({ message: "This is the API route." });
+});
+
+// Define a route with URL parameters
+app.get("/greet/:name", (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}!`);
+});
+
+// Start the server on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
