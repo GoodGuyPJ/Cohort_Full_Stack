9f4529a454e57765686bf7a98f3355ae6c2160ce
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-03-15 23:18:08 +0530

    commit

--- JSPractice/basic/index.js
+++ JSPractice/basic/index.js
@@ -2,7 +2,7 @@
 // const, let, var
 // var is function scoped and let is block scoped and const is block scoped
 // var can be redeclared and redefined but let and const can't be redeclared but can be redefined
-console.log("Hello World");
+// console.log("Hello World");
 
-async with await
+// async with await
  
