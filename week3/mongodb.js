842afd62880aff3a9c368f6788d0e96a83349dbb
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-03-20 18:15:28 +0530

    mongodb

--- week3/mongodb.js
+++ week3/mongodb.js
@@ -4,34 +4,38 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://admin:Push12Jha@cluster0.lpba2sg.mongodb.net/userappnew")
+mongoose.connect(
+  "mongodb+srv://admin:Push12Jha@cluster0.lpba2sg.mongodb.net/userappnew"
+);
 
-const User = mongoose.model("User", {name: String, email: String, password: String});
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  password: String,
+});
 
-app.post("/signup", async function(req, res){
+app.post("/signup", async function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
   const name = req.body.name;
 
+  const existingUser = await User.findOne({ email: username });
+  if (existingUser) {
+    return res.status(400).send("User already exists");
+  }
 
-const existingUser = await User.findOne({ email: username });
-if(existingUser){
-  return res.status(400).send("User already exists");
-}
+  const user = new User({
+    name: name,
+    email: username,
+    password: password,
+  });
 
-const user = new User({
-  name: name,
-  email: username,
-  password: password
+  user.save();
+  res.json({
+    msg: "user created successfully",
+  });
 });
 
-
-user.save();
-res.json({
-  "msg":"user created successfully"
-})
-})
-
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Server started");
-})
+});
