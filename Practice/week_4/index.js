f69d057679d715a74ea73b455ad3d3299cf32c2c
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-03-22 23:49:49 +0530

    Database

---
+++ Practice/week_4/index.js
@@ -0,0 +1,4 @@
+//what is database?
+// It is a place where we can store data in an organized way. It is a collection of data that is stored in a computer system. Databases are used to store, retrieve, and manage data. It is a structured set of data held in a computer, especially one that is accessible in various ways.
+
+// servers autoscale to handle the load, and the database is the bottleneck. The database is
