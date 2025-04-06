ff42fcaebd31b49d536f8e1a66147cf7e63a48d7
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-04-06 16:28:44 +0530

    commit

--- ZPractice_Complete/JavaScript/index.js
+++ ZPractice_Complete/JavaScript/index.js
@@ -3,7 +3,31 @@ console.log("Practice JavaScript");
 
 // var , let, const -> used for declaring variables
 
-// javascript is a versatile and widely-used programming language. It is an essential part of web development and is used in a variety of applications, from building websites to creating interactive user interfaces. Learning JavaScript can open up a world of possibilities for your coding skills and career opportunities. Here are some reasons why you should learn JavaScript: 
+// javascript is a versatile and widely-used programming language. It is an essential part of web development and is used in a variety of applications, from building websites to creating interactive user interfaces. Learning JavaScript can open up a world of possibilities for your coding skills and career opportunities. Here are some reasons why you should learn JavaScript:
 // 1. JavaScript is the language of the web: JavaScript is the most widely-used programming language for web development. It is used to create interactive and dynamic websites, and is an essential skill for front-end developers.
 
-for(let i)
+// 1. create a counter in javascript (counts down form 30 ot 0)
+let counter = 1;
+// let countdown = setInterval(() => {
+//   console.clear();
+//   console.log(counter);
+//   counter--;
+//   if (counter < 0) {
+//     clearInterval(countdown);
+//   }
+// }, 1000);
+
+//2 calculate the time it takes between a setTimeout and the inner function actually running
+
+let startTime = new Date().getTime();
+setTimeout(() => {
+  let endTime = new Date().getTime();
+  console.log(`The setTimeout took ${endTime - startTime} milliseconds to run`);
+});
+
+//3. create a terminal clock (hh:mm:ss) that updates every second
+let clock = setInterval(() => {
+  let date = new Date();
+  console.clear();
+  console.log(date.toLocaleTimeString());
+}, 1000);
