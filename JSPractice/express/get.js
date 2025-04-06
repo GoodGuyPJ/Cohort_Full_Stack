5d7a918cd232846cee97e585720931723a56b28c
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-04-06 20:45:01 +0530

    anagram completed

--- JSPractice/express/get.js
+++ JSPractice/express/get.js
@@ -2,10 +2,32 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Define a simple GET route
-app.get("/", (req, res) => {
-  res.send("Hello, this is a GET request!");
+// HTTP Methods
+// GET - Retrieve data
+// POST - Create data
+// PUT - Update data
+// DELETE - Delete data
+// PATCH - Partially update data
+app.get("/ab?cd", (req, res) => {
+  res.send("If the user hit (acd) or (abcd) then this route will be hit");
 });
+
+//Regex
+app.get(/x?cd/, (req, res) => {
+  res.send("If the path includes the letter x then this route will be hit");
+});
+
+//req, res, next
+app.get(
+  "/next",
+  (req, res, next) => {
+    console.log("Middleware function executed");
+    next(); // Call the next middleware or route handler
+  },
+  (req, res) => {
+    res.send("Hello from the next route!");
+  }
+);
 
 // Define a route with a parameter
 app.get("/greet/:name", (req, res) => {
--- assignments-master/01-js/easy/anagram.js
+++ assignments-master/01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  str1 = str1.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  str2 = str2.replace(/[^a-zA-Z]/g, "").toLowerCase();
 
+  const sortedStr1 = str1.split("").sort().join("");
+  const sortedStr2 = str2.split("").sort().join("");
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
