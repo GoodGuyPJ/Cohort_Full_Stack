c0754039df5ef6f4c642fd0d9843b6af1acd038b
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-05-05 23:34:52 +0530

    typescript learn

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -30,3 +30,36 @@ let isTrue: boolean = true;
 
 let isFalse: boolean = false;
 console.log(isTrue); // Output: true
+
+// Type Inference -> is a feature in TypeScript that allows the compiler to automatically infer the type of a variable based on its initial value. This means that you don't have to explicitly declare the type of a variable if the compiler can determine it from the value assigned to it.
+
+let tech = "JavaScript"; // TypeScript infers the type as string
+let tech2 = 10; // TypeScript infers the type as number
+let tech3 = true; // TypeScript infers the type as boolean
+let tech4 = null; // TypeScript infers the type as null
+console.log(typeof tech); // Output: string
+
+
+// Any Type-> has a special type called "any" that allows you to assign any value to a variable without TypeScript checking the type. This is useful when you don't know the type of a variable at compile time or when you're working with dynamic data.
+let myVar: any = "Hello"; // myVar is of type any
+myVar = 10;
+myVar();
+myVar = true;
+myVar.toUpperCase();
+console.log(myVar)
+
+
+//Function Parameter Annotations
+// Function parameter annotations are used to specify the types of parameters that a function can accept. This helps TypeScript check the types of arguments passed to the function and provides better type safety.
+
+function addOne(a: number, b: number): number {
+  return a + b;
+} 
+
+
+//Arrow function
+const double = (x: number, y: number) => x*y;
+const res = double(2,3);
+console.log(res); // Output: 6
+
+//
