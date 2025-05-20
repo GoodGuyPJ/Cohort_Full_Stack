7dc8cbea295eb71a708f74dc5bac9da7d8417c8a
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-05-20 21:42:52 +0530

    graphQL

--- GraphQL/index.js
+++ GraphQL/index.js
@@ -1,10 +1,46 @@
+// import { ApolloServer } from "@apollo/server";
+// import { startStandaloneServer } from "@apollo/server/standalone";
+
+// const server = new ApolloServer({
+//   typeDefs: `#graphql
+//     type Query {
+//       hello: String
+//     }
+
+//   `,
+//   resolvers: {
+//     Query: {
+//       hello: () => "Hello world!",
+//     },
+//   },
+// })
+
+// const { url } = await startStandaloneServer(server, { listen: { port: 7000}});
+// console.log(`🚀  Server ready at: ${url}`);
+
+//********************* */
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+  typeDefs: `#graphql
+    type Query {
+      greetings(name: String): String!
+      add(a: Int!, b: Int!): Int
+    }
+   
+  `,
+  resolvers: {
+    Query: {
+      greetings(parent, args, ctx, info) {
+        const { name } = args;
+        return `Hello ${name || "World"}!`;
+      },
 
-const { url } = await startStandaloneServer(server, { listen: { port: 4000}});
+      add: (_, { a, b }) => a + b,
+    },
+  },
+});
+
+const { url } = await startStandaloneServer(server, { listen: { port: 7000 } });
 console.log(`🚀  Server ready at: ${url}`);
