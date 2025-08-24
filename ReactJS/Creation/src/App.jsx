30c49f5ebe5b0be4403ebd556cc794994d6afd31
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-08-24 12:24:47 +0530

    styles in React

--- ReactJS/Creation/src/App.jsx
+++ ReactJS/Creation/src/App.jsx
@@ -12,7 +12,20 @@ function App() {
       {/* <Greet /> */}
       {/* <UserList /> */}
       {/* <Props /> */}
-      <ConditionStatments isValid={true} />
+      {/* <ConditionStatments isValid={true} /> */}
+
+      <h1
+        style={{
+          color: "red",
+          backgroundColor: "Blue",
+          padding: "20px",
+          width: "50%",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        Inline styles
+      </h1>
     </section>
   );
 }
