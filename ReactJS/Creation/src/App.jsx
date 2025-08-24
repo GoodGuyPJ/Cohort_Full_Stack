c8c7bb2ea14ed533f3631ec34baff876ecac9d28
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-08-24 13:07:56 +0530

    Styles and React Icons

--- ReactJS/Creation/src/App.jsx
+++ ReactJS/Creation/src/App.jsx
@@ -6,6 +6,12 @@ import UserList from "./components/UserList";
 import Props from "./components/Props";
 import ConditionStatments from "./ConditionStatments";
 
+// React icons
+import { ImAmazon } from "react-icons/im";
+import { ImBrightnessContrast } from "react-icons/im";
+import { ImCool } from "react-icons/im";
+import { FaBeerMugEmpty } from "react-icons/fa6";
+
 function App() {
   return (
     <section className="root">
@@ -26,6 +32,23 @@ function App() {
       >
         Inline styles
       </h1>
+
+      {/* React Icons */}
+      <h3>React Icons: Examples</h3>
+      <div style={{ fontSize: "100px", padding: "20px" }}>
+        <ImAmazon style={{ fontSize: "100px", padding: "20px" }} />
+        <ImBrightnessContrast style={{ fontSize: "100px", padding: "20px" }} />
+        <ImCool style={{ fontSize: "100px", padding: "20px" }} />
+      </div>
+
+      <div>
+        <h2>
+          Chears:
+          <p style={{ fontSize: "150px", color: "brown" }}>
+            <FaBeerMugEmpty onClick={() => alert("chears boys")} />
+          </p>
+        </h2>
+      </div>
     </section>
   );
 }
