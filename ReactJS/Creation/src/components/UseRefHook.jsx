7dc74ca62e730459cb457fee2534b3b27632c4dd
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-08-27 22:12:30 +0530

    useRef Hook

--- ReactJS/Creation/src/components/UseRefHook.jsx
+++ ReactJS/Creation/src/components/UseRefHook.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const UseRefHook = () => {
   const inputElement = useRef(null);
@@ -9,10 +9,29 @@ const UseRefHook = () => {
     inputElement.current.value = "1000";
   };
 
+  const [count, setCount] = useState(0);
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
   return (
     <div>
       <input type="text" ref={inputElement} />
       <button onClick={() => focusInput()}>Focus & write PJ</button>
+
+      <h1>******************</h1>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => clearInterval(intervalRef.current)}>
+        Stop Counter
+      </button>
     </div>
   );
 };
