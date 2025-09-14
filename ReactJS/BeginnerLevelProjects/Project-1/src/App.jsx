d0ce50f5472c4ac7ec1dccf274617bfa98b9fc0e
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-09-14 23:56:48 +0530

    ToggleBackgroundColor

--- ReactJS/BeginnerLevelProjects/Project-1/src/App.jsx
+++ ReactJS/BeginnerLevelProjects/Project-1/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Counter from "./components/Counter";
 import Todos from "./components/Todos";
 import Calculator from "./components/Calculator";
+import ToggleBackgroundColor from "./components/ToggleBackgroundColor";
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       {/* <Todos /> */}
-      <Calculator />
+      {/* <Calculator /> */}
+      <ToggleBackgroundColor />
     </div>
   );
 };
