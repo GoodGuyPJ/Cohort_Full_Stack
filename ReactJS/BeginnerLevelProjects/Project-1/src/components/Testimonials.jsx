bc4ade931d74bcddf0e19981d8a6dd0e5ea14d40
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-09-23 23:43:22 +0530

    react testimonials

---
+++ ReactJS/BeginnerLevelProjects/Project-1/src/components/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Testimonials = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Testimonials
